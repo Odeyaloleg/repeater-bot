63baa6ef7e5f88d20ebd3395f30da2986fa883ab
Odeyaloleg <Odeyaloleg@gmail.com>
2020-03-26 18:36:46 +0700

    Added possibility to use "#" in config for comments and empty lines for readability.

--- src/Config.hs
+++ src/Config.hs
@@ -5,7 +5,7 @@ module Config
 import           System.IO (FilePath)
 import           Control.Exception ( SomeException, try )
 import qualified Data.ByteString.Char8 as BS8 ( ByteString, putStrLn, readFile, pack, lines
-                                              , split, map, append, head, tail, unpack )
+                                              , split, map, append, head, tail, unpack, null )
 import qualified Data.Map.Strict as MS ( Map, fromList )
 
 readConfig :: FilePath -> IO (Either String (MS.Map BS8.ByteString BS8.ByteString))
@@ -18,7 +18,8 @@ readConfig configName = do
 -- Seems like it's way more complicated than it can be
 parseConfig :: BS8.ByteString -> MS.Map BS8.ByteString BS8.ByteString
 parseConfig contents =
-  let settingsLines = fmap (tuplesOn '=') $ BS8.lines contents in
+  let dataLines = filter (\line -> if BS8.null line || BS8.head line == '#' then False else True) (BS8.lines contents)
+      settingsLines = fmap (tuplesOn '=') $ dataLines in
   MS.fromList settingsLines
   where tuplesOn c settingData = helper c (BS8.pack "",settingData)
         helper c (fieldName,rest) | BS8.head rest == c = (fieldName, BS8.tail rest)
