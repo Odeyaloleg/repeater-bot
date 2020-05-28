0fb4a66aa1141b004589941ccc5627d20d7ab161
Odeyaloleg <Odeyaloleg@gmail.com>
2020-05-28 10:00:57 +0700

    Making tests compatible with latest revision.

--- test/Tests/Slack/Settings.hs
+++ test/Tests/Slack/Settings.hs
@@ -4,13 +4,14 @@ module Tests.Slack.Settings
   ( runTests
   ) where
 
-import qualified Data.Map.Strict as MS
 import qualified Data.ByteString.Char8 as BS8
+import qualified Data.Map.Strict as MS
+import Logger (LogLevel(..))
+import Settings (setBotSettings)
 import Slack.Settings
-  ( SlackSettings(..)
-  , ServerSettings(..)
+  ( ServerSettings(..)
+  , SlackSettings(..)
   , SlackTextAnswers(..)
-  , setSlackSettings
   )
 import Test.QuickCheck (Property, quickCheckAll)
 import Test.QuickCheck.Monadic (assert, monadicIO, run)
@@ -19,21 +20,25 @@ prop_correctData :: Property
 prop_correctData =
   monadicIO $ do
     let result =
-          setSlackSettings $
+          setBotSettings $
           (MS.fromList
-            [ ("SlackToken", "BotToken")
-            , ("ServerIP", "127.0.0.1")
-            , ("ServerPort", "3000")
-            , ("CommandHelp", "HelpMsg")
-            , ("CommandRepeat", "RepeatMsg")
-            ] :: MS.Map BS8.ByteString BS8.ByteString)
+             [ ("SlackToken", "BotToken")
+             , ("ServerIP", "127.0.0.1")
+             , ("ServerPort", "3000")
+             , ("RepetitionsNumber", "1")
+             , ("CommandHelp", "HelpMsg")
+             , ("CommandRepeat", "RepeatMsg")
+             , ("SlackLogLevel", "DEBUG")
+             ] :: MS.Map BS8.ByteString BS8.ByteString)
     assert $
       result ==
       Just
         (SlackSettings
            "BotToken"
            (ServerSettings "127.0.0.1" 3000)
-           (SlackTextAnswers "HelpMsg" "RepeatMsg"))
+           1
+           (SlackTextAnswers "HelpMsg" "RepeatMsg")
+           LevelDEBUG)
 
 return []
 
